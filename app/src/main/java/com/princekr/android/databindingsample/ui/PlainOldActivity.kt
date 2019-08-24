6594e3a25408ec2e6124bc908fa017184a1161e5
prince <prince18pat@gmail.com>
2019-08-25 02:05:12 +0900

    add onlick event and update color of image

--- app/src/main/java/com/princekr/android/databindingsample/ui/PlainOldActivity.kt
+++ app/src/main/java/com/princekr/android/databindingsample/ui/PlainOldActivity.kt
@@ -1,13 +1,16 @@
 package com.princekr.android.databindingsample.ui
 
 import android.content.Context
+import android.content.res.ColorStateList
 import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.view.View
 import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.core.widget.ImageViewCompat
 import androidx.lifecycle.ViewModelProviders
 import com.princekr.android.databindingsample.R
 import com.princekr.android.databindingsample.data.Popularity
@@ -28,6 +31,16 @@ class PlainOldActivity : AppCompatActivity() {
 
         // TODO: Explicitly setting initial values is a bad pattern. We'll fix that.
         updateName()
+        updateLikes()
+    }
+
+    /**
+     * This method is triggered by the `android:onClick` attribute in the layout. It puts business
+     * logic in the activity, which is not ideal. We should do something about that.
+     */
+    fun onLike(view: View) {
+        viewModel.onLike()
+        updateLikes()
     }
 
     /**
@@ -47,10 +60,26 @@ class PlainOldActivity : AppCompatActivity() {
     private fun updateLikes() {
         findViewById<TextView>(R.id.likes).text = viewModel.likes.toString()
         findViewById<ProgressBar>(R.id.progressBar).progress = (viewModel.likes * 100 / 5).coerceAtLeast(100)
+
         val image = findViewById<ImageView>(R.id.imageView)
+
+        val color = getAssociatedColor(viewModel.popularity, this)
+
+        ImageViewCompat.setImageTintList(image, ColorStateList.valueOf(color))
 
         //val color = ge
         image.setImageDrawable(getDrawablePopularity(viewModel.popularity, this))
+    }
+
+    private fun getAssociatedColor(popularity: Popularity, context: Context): Int {
+        return when (popularity) {
+            Popularity.NORMAL -> context.theme.obtainStyledAttributes(intArrayOf(android.R.attr.colorForeground)).getColor(
+                0,
+                0x000000
+            )
+            Popularity.POPULAR -> ContextCompat.getColor(context, R.color.popular)
+            Popularity.STAR -> ContextCompat.getColor(context, R.color.star)
+        }
     }
 
     private fun getDrawablePopularity(popularity: Popularity, context: Context): Drawable? {
