b1f6a1a8c50dd4fe09883bbd2995a77f98cec315
prince <prince18pat@gmail.com>
2019-08-25 17:35:26 +0900

    add binding adapters and live data

--- app/src/main/java/com/princekr/android/databindingsample/data/SimpleViewModel.kt
+++ app/src/main/java/com/princekr/android/databindingsample/data/SimpleViewModel.kt
@@ -1,36 +1,35 @@
 package com.princekr.android.databindingsample.data
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 
 /**
  * A simple VM for [com.princekr.android.databindingsample.ui.PlainOldActivity].
  */
 class SimpleViewModel : ViewModel() {
-    val name = "Grace"
-    val lastName = "Hopper"
 
-    var likes = 0
-        private set // This is to prevent external modification of the variable.
+    private val _name = MutableLiveData("Ada")
+    private val _lastName = MutableLiveData("lovelace")
+    private val _likes = MutableLiveData(0)
 
-    /**
-     * Increments the number of likes.
-     */
+    val name: LiveData<String> = _name
+    val lastName: LiveData<String> = _lastName
+    val likes: LiveData<Int> = _likes
+
     fun onLike() {
-        likes++
+        _likes.value = (_likes.value ?: 0) + 1
     }
 
-    /**
-     * Returns the popularity in buckets: [Popularity.NORMAL], [Popularity.POPULAR], [Popularity.STAR]
-     */
-    val popularity: Popularity
-        get() {
-            return when {
-                likes > 9 -> Popularity.STAR
-                likes > 4 -> Popularity.POPULAR
-                else -> Popularity.NORMAL
-            }
+    // popularity is exposed as LiveData using a Transformation instead of @Bindable property.
+    val popularity: LiveData<Popularity> = Transformations.map(_likes) {
+        when {
+            it > 9 -> Popularity.STAR
+            it > 4 -> Popularity.POPULAR
+            else -> Popularity.NORMAL
         }
-
+    }
 }
 
 enum class Popularity {
